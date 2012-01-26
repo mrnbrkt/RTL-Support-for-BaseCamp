5dc20623b77a92def58f979d015dc17b98ca76de
Meron Bareket <meron@solver.co.il>
2012-01-26 20:34:01 +0200

    fixed support of ul/li in redirectioning by JS

--- contentscript.js
+++ contentscript.js
@@ -3,6 +3,11 @@ function setCssForLTR()
 	$('#comments, #message_header, #OriginalPost, .text_area, .message, .check_box_commentable, #preview_body').css({
 		"direction": "ltr",
 		"text-align": "left" });
+	$('div.formatted_text_body li, div.formatted_text_body ul, #preview_body li, #preview_body ul').css( { 
+	"margin-left": "15px", 
+	"padding-left": "3px", 
+	"padding-right": "0px",
+	"margin-right": "0" });
 }
 
 function setCssForRTL()
@@ -10,6 +15,11 @@ function setCssForRTL()
 	$('#comments, #message_header, #OriginalPost, .text_area, .message, .check_box_commentable, #preview_body').css({
 		"direction": "rtl",
 		"text-align": "right" });
+	$('div.formatted_text_body li, div.formatted_text_body ul, #preview_body li, #preview_body ul').css( { 
+		"margin-right": "15px", 
+		"padding-right": "3px", 
+		"padding-left": "0px",
+		"margin-left": "0" });
 }
 function toggleCssOfDirectioning(shouldBeLTR)
 {
